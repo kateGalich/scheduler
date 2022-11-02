92a5369e0f28d19481ff87dc0e351d8803e224f0
Kate Galich <kate.galich@outlook.com>
2022-11-02 19:43:28 -0400

    Adding dayList

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,13 +8,25 @@ export default function DayListItem(props) {
     "day-list__item--full": !props.spots
   });
 
+  const formatSpots = function() {
+    let result = `${props.spots} spots remaining`;
+    
+    if (props.spots === 0) {
+      result = "no spots remaining";
+    } else if (props.spots === 1) {
+      result = "1 spot remaining";
+    }
+
+    return result;
+  };
+
   return (
     <li
       onClick={() => props.setDay(props.name)}
       className={dayClass}
     >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
