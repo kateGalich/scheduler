0e7fa5f2bc3e9fcc8ad72070495a84977c3d20a2
Kate Galich <kate.galich@outlook.com>
2022-11-03 14:25:22 -0400

    Adding interviewerList

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import classNames from "classnames";
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+  const listItems = props.interviewers.map((interviewer) =>
+    <InterviewerListItem
+      key={interviewer.id}
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={props.interviewer === interviewer.id}
+      setInterviewer={props.setInterviewer}
+    />
+  );
+  return (
+
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {listItems}
+      </ul>
+    </section>
+
+  );
+}
