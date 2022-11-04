41370863419a54e2e1d41665ef8359dfd255fdec
Kate Galich <kate.galich@outlook.com>
2022-11-03 21:28:42 -0400

    Adding Confirm Component

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 
 
@@ -143,6 +144,7 @@ storiesOf("InterviewerList", module)
   ));
 
 const student = "Lydia Miller-Jones";
+const message = "Delete the appointment?";
 
 storiesOf("Appointment", module)
   .addParameters({
@@ -158,8 +160,13 @@ storiesOf("Appointment", module)
       interviewer={interviewer}
       onEdit={action("onEdit")}
       onDelete={action("onDelete")}
-    />
-
-  );
+    />)
+  .add("Confirm", () =>
+    <Confirm
+      message={message}
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
+    />)
+  ;
 
 
